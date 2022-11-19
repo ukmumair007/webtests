afabc4fe85df09b48a4f58bea123b4010707f9c1
suhailkhn112 <91205651+suhailkhn112@users.noreply.github.com>
2022-11-19 12:13:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert("Hacker 1337");
+let req = new XMLHttpRequest();
+req.responseType = "document";
+req.open("GET", "http://testphp.vulnweb.com/userinfo.php");
+req.addEventListener("load", function () {
+	let res = req.responseXML;
+	alert(res);
+});
+req.send();
