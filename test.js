67b731cdbb7e552c5041ce7345d8c5a35691332b
suhailkhn112 <91205651+suhailkhn112@users.noreply.github.com>
2023-08-09 10:51:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-let req = new XMLHttpRequest();
-req.responseType = "document";
-req.open("GET", "http://testphp.vulnweb.com/userinfo.php");
-req.addEventListener("load", function () {
-	let res = req.responseXML;
-	console.log(res);
-	alert(res.querySelector("#content > div:nth-child(3) > form > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text]").value);
-});
-req.send();
+alert(1);
